61adc5a6170ad93808f3231b455cea4a7f66559f
Elena Tsvetkova <hellenatsvetkova@gmail.com>
2025-02-16 11:02:59 +0200

    Delete main_app directory

--- main_app/choices.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-class GenreChoices(models.TextChoices):
-    FICTION = 'Fiction', 'Fiction'
-    NON_FICTION = 'Non-Fiction', 'Non-Fiction'
-    SCIENCE_FICTION = 'Science Fiction', 'Science Fiction'
-    HORROR = 'Horror', 'Horror'
